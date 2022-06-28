4af3efa3cf2700aaaafcb8f885d09a86f036f1eb
Brega Evgeny <eabrega@gmail.com>
2022-06-28 22:51:41 +0300

    Добавьте файлы проекта.

---
+++ BrothersPilots/Program.cs
@@ -0,0 +1,24 @@
+using System;
+//using System.Device.Gpio;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BrothersPilots
+{
+    //private static GpioController _GpioController;
+    //private static double _battVoltage = 0;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
